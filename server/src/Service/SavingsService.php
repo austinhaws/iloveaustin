882c59fa3d084e7ed88d8546502a7326c38b0ac3
Austin Haws <ahaws@utah.gov>
2019-10-08 13:55:03 -0600

    Add savings mutations

--- server/src/Service/SavingsService.php
+++ server/src/Service/SavingsService.php
@@ -9,4 +9,19 @@ class SavingsService extends BaseService
 		//todo: use real account id/token from args
 		return $this->context->daos->savings->selectSavingsByAccountId(2);
 	}
+
+	public function saveSavings($rootValue, $args, array $savings)
+	{
+		// todo: if has id then unset account_id - make sure that unset account_id doesn't update to null
+		// todo: if no id then set to current account id
+		$savings['account_id'] = 2;
+		return $this->context->daos->savings->saveSavings($savings);
+	}
+
+	public function deleteSavings($rootValue, $args, int $savingsId)
+	{
+		// todo: select record and make sure the account id is the current account id
+		$this->context->daos->savings->deleteSavings($savingsId);
+		return $savingsId;
+	}
 }
---
+++ server/src/Type/Input/SavingsInputType.php
@@ -0,0 +1,30 @@
+<?php
+namespace ILoveAustin\Type\Input;
+
+use ILoveAustin\Types;
+
+class SavingsInputType extends BaseInputType
+{
+    public function __construct()
+    {
+    	parent::__construct([
+			self::NAME => 'Savings Input',
+			self::DESCRIPTION => 'Savings data',
+			self::FIELDS => function() {
+				return [
+					'id' => Types::id(),
+					'amountGoal' => Types::string(),
+					'amountCurrent' => Types::string(),
+					'name' => Types::string(),
+					'dueDate' => Types::string(),
+					'notes' => Types::string(),
+				];
+			},
+			self::FIELD_CONVERSIONS => [
+				'amountGoal' => 'amt_goal',
+				'amountCurrent' => 'amt_current',
+				'dueDate' => 'due_date',
+			],
+		]);
+    }
+}
