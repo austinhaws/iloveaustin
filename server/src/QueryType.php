dbb283e1cd3ef665d74f3d256d4a07b4978cde09
Austin Haws <ahaws@utah.gov>
2019-10-14 15:53:59 -0600

    Remove selecting all accounts
    It's not secure and not needed.

--- server/src/QueryType.php
+++ server/src/QueryType.php
@@ -17,11 +17,6 @@ class QueryType extends ObjectType
         $config = [
             'name' => 'Query',
             'fields' => [
-            	'accounts' => [
-            		'type' => Types::listOf(Types::account()),
-					'description' => 'Returns accounts',
-					'args' => [],
-				],
 				'monthlies' => [
 					'type' => Types::listOf(Types::monthly()),
 					'description' => 'Returns account monthly',
@@ -60,11 +55,6 @@ class QueryType extends ObjectType
         ];
         parent::__construct($config);
     }
-
-    public function accounts($rootValue, $args)
-	{
-		return $this->context->services->account->selectAccounts($rootValue, $args);
-	}
 
     public function monthlies($rootValue, $args)
 	{
