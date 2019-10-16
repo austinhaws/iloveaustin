db98998597501051e9e896e2e2d01171a300d80a
Austin Haws <ahaws@utah.gov>
2019-10-16 15:58:24 -0600

    Make period date picker editable

--- client/src/js/iloveaustin/monthly/MonthlyDatePicker.jsx
+++ client/src/js/iloveaustin/monthly/MonthlyDatePicker.jsx
@@ -2,23 +2,32 @@ import '@babel/polyfill';
 import React from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
-import {withStyles} from "@material-ui/core";
+import {CircularProgress, withStyles} from "@material-ui/core";
 import * as PropTypes from "prop-types";
 import Button from "@material-ui/core/Button";
 import Styles from "../../app/Styles";
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Period from "../../app/period/Period";
+import MaskedInput from 'react-text-mask';
+import {joinClassNames} from "dts-react-common";
+import moment from "moment";
 
 const propTypes = {
+	app: PropTypes.object.isRequired,
 	iLoveAustin: PropTypes.object.isRequired,
 };
 const defaultProps = {};
 const mapStateToProps = state => ({
+	app: state.app,
 	iLoveAustin: state.iLoveAustin,
 });
 
 class MonthlyDatePicker extends React.Component {
+
+	changePeriod = e => e.target.value.replace('_', '').length === 7 &&
+		moment(e.target.value, 'MM/YYYY').isValid() &&
+		Period.moveToPeriod(e.target.value);
 
 	render() {
 		if (!this.props.iLoveAustin.periods) {
@@ -30,7 +39,14 @@ class MonthlyDatePicker extends React.Component {
 			<div className={classes.root}>
 				<h3 className={classes.sectionTitle}>
 					<Button onClick={() => Period.moveToPeriod(this.props.iLoveAustin.periods.previousPeriod)}><ChevronLeftIcon fontSize="small"/></Button>
-					{this.props.iLoveAustin.periods.period}
+					<MaskedInput
+						className={joinClassNames(classes.inputSizeMedium, classes.textAlignCenter, classes.dateTimePickerInput)}
+						mask={[/\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
+						value={this.props.iLoveAustin.periods.period}
+						onChange={this.changePeriod}
+						disabled={this.props.app.ajaxSpinnerCount}
+					/>
+					{this.props.app.ajaxSpinnerCount ? <CircularProgress size={20}/> : undefined}
 					<Button onClick={() => Period.moveToPeriod(this.props.iLoveAustin.periods.nextPeriod)}><ChevronRightIcon fontSize="small"/></Button>
 				</h3>
 			</div>
